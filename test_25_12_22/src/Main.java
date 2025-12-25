c4ea7aaccc598a52b9f9648695f37d65c0a4c859
liyongzhenkk <liyongzhen04@gmail.com>
2025-12-25 10:19:37 +0800

    test25_12_22

---
+++ test_25_12_22/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("Hello World!");
+        System.out.println("hello ");
+
+    }
+}
+
+class test2{
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(20);
+        System.out.println(10.5);
+        System.out.println(true);
+
+    }
+}
