1e655bf11e5263a8208deb6ad78b78e5495f81a6
liyongzhenkk <liyongzhen04@gmail.com>
2025-12-26 09:19:34 +0800

    init project

---
+++ test_25_12_26/src/Test.java
@@ -0,0 +1,27 @@
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class Test {
+    public static void main2() {
+        System.out.println("++++++++++++++++++++");
+    }
+
+    public static void main(String[] args) {
+        
+        int i = 10;
+        while (i < 100) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+//        int i = 1;
+//        while(i < 10) {
+//            main2();
+//            if(i % 3 == 0) {
+//                System.out.println("finded !!!!!!!!");
+//                break;
+//            }
+//            i++;
+//        }
+    }
+}
